ab7d76679e4baa995cbc1703280358b00f877d0d
Nghị <nghingothanhnghi@gmail.com>
2025-09-25 16:19:57 +0700

    update

--- app/jackpot/services/ticket_service.py
+++ app/jackpot/services/ticket_service.py
@@ -4,21 +4,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from app.jackpot.models.draw import Ticket, PlayType, Draw
 from datetime import datetime
-from app.jackpot.services.draw_service import draw_service
 class TicketService:
-
-    # def create_ticket(self, db: Session, user_id: int, numbers: list[int], play_type: PlayType, draw_id: int) -> Ticket:
-    #     if play_type != PlayType.basic and len(numbers) < int(play_type[-1]):
-    #         raise ValueError("Not enough numbers for selected Bao play type.")
-    #     try:
-    #         ticket = Ticket(user_id=user_id, numbers=numbers, play_type=play_type, draw_id=draw_id)
-    #         db.add(ticket)
-    #         db.commit()
-    #         db.refresh(ticket)
-    #         return ticket
-    #     except SQLAlchemyError as e:
-    #         db.rollback()
-    #         raise e
 
     def create_ticket(
         self, 
@@ -28,32 +14,6 @@ class TicketService:
         play_type: PlayType, 
         draw_id: Optional[int] = None
     ) -> Ticket:
-        # """
-        # Create a ticket for the user.
-        # If draw_id is not given, attach to the current or next active draw.
-        # """
-        # if play_type != PlayType.basic and len(numbers) < int(play_type[-1]):
-        #     raise ValueError("Not enough numbers for selected Bao play type.")
-
-        # try:
-        #     # pick or create current draw if not explicitly given
-        #     if draw_id is None:
-        #         draw = draw_service.get_or_create_current_draw(db)
-        #         draw_id = draw.id
-
-        #     ticket = Ticket(
-        #         user_id=user_id, 
-        #         numbers=numbers, 
-        #         play_type=play_type, 
-        #         draw_id=draw_id
-        #     )
-        #     db.add(ticket)
-        #     db.commit()
-        #     db.refresh(ticket)
-        #     return ticket
-        # except SQLAlchemyError as e:
-        #     db.rollback()
-        #     raise e
         # ✅ 1. Resolve which draw this ticket should belong to
         if draw_id is None:
             # Find the next available draw (future draw_date)
