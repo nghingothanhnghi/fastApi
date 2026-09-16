27a4aabb6501fd1308d5a7860ae791a1abdde051
Nghị <nghingothanhnghi@gmail.com>
2026-09-16 14:43:34 +0700

    update

--- app/ai_vision/models/__init__.py
+++ app/ai_vision/models/__init__.py
@@ -5,9 +5,10 @@ from .vision_prediction import VisionPrediction
 from .plant_growth import PlantGrowthRecord
 from .plant_health import PlantHealthRecord
 from .plant_anomaly import PlantAnomaly
+from .anomaly_tracker import AnomalyTracker
 from .ai_recommendation import AIRecommendation
 
 __all__ = [
     "VisionPlant", "VisionCamera", "PlantImage", "AIInferenceJob", "VisionPrediction",
-    "PlantGrowthRecord", "PlantHealthRecord", "PlantAnomaly", "AIRecommendation",
+    "PlantGrowthRecord", "PlantHealthRecord", "PlantAnomaly", "AnomalyTracker", "AIRecommendation",
 ]
